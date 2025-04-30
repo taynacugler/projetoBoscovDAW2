484848e0a047976aa402c4abbfb6737507605953
Tayna Cugler <taynacardosocugler@gmail.com>
2025-04-30 19:58:30 -0300

    refactor: clean up comments and formatting in Prisma schema and module files

--- backend-boscov/src/app.module.ts
+++ backend-boscov/src/app.module.ts
@@ -3,12 +3,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsuarioModule } from './usuario/usuario.module'; // Importe o seu módulo de usuário
-import { PrismaService } from './prisma/prisma.service'; // Importe o PrismaService
+import { UsuarioModule } from './usuario/usuario.module'; 
+import { PrismaService } from './prisma/prisma.service'; 
 
 @Module({
-  imports: [UsuarioModule], // Você já tem outros módulos aqui
+  imports: [UsuarioModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService], // Aqui você adiciona o PrismaService aos providers
+  providers: [AppService, PrismaService], 
 })
 export class AppModule {}
--- backend-boscov/src/usuario/usuario.module.ts
+++ backend-boscov/src/usuario/usuario.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsuarioController } from './usuario.controller';
 import { UsuarioService } from './usuario.service';
-import { PrismaModule } from '../prisma/prisma.module'; // importa o PrismaModule
+import { PrismaModule } from '../prisma/prisma.module'; 
 import { PrismaService } from 'src/prisma/prisma.service';
 
 
